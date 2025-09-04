9143ff7512d15fe6723f1c17cb13e69282a4a964
rena <166546709+RENATALB@users.noreply.github.com>
2025-09-04 11:34:03 -0300

    trying

--- TodoList.js
+++ TodoList.js
@@ -1,16 +1,25 @@
-let Lista = document.getElementById("Lista");
-let i;
-
 function agregarElementos() {
     let li = document.createElement("li");
     let valorInput = document.getElementById("Input").value;
     let inputText = document.createTextNode(valorInput);
     li.appendChild(inputText);
-        if (valorInput === '') {
-            alert("El valor del item no puede ser nulo")
-        } else {
-            document.getElementById("Lista").appendChild(li);
-        }
 
+    if (valorInput === '') {
+        alert("El valor del item no puede ser nulo");
+    } else {
+        Lista.appendChild(li);
+    }
+    document.getElementById("Input").value = "";
 }
-document.getElementById("Input").value = "";
+    // 🔥 Agregamos botón Cerrar
+    let span = document.createElement("SPAN");
+    let txt = document.createTextNode("×"); // símbolo de cierre
+    span.className = "Cerrar";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    // 🔥 Asignamos evento al nuevo botón
+    span.onclick = function() {
+        let div = this.parentElement;
+        div.style.display = "none";
+    }
